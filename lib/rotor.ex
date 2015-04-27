28aa64f5a30f17621bc30c035f24c0d1b579e0d6
Kerry Buckley <kerryjbuckley@gmail.com>
2015-04-27 21:21:10 +0100

    Switch to one-line function definitions

--- lib/rotor.ex
+++ lib/rotor.ex
@@ -1,17 +1,9 @@
 defmodule Rotor do
-  def translate_left mapping, position do
-    Enum.at(mapping, position) - ?A
-  end
+  def translate_left(mapping, position), do: Enum.at(mapping, position) - ?A
 
-  def reflect mapping, position do
-    translate_left mapping, position
-  end
+  def reflect(mapping, position), do: translate_left(mapping, position)
 
-  def translate_right mapping, position do
-    Enum.find_index mapping, &(&1 - ?A == position)
-  end
+  def translate_right(mapping, position), do: Enum.find_index(mapping, &(&1 - ?A == position))
 
-  def rotate [head|tail] do
-    tail ++ [head]
-  end
+  def rotate([head|tail]), do: tail ++ [head]
 end

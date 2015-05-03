878f5f65abebc20f3081b145e1e74acb7e15eb40
Kerry Buckley <kerryjbuckley@gmail.com>
2015-05-03 15:11:41 +0100

    Translate left when rotated

--- lib/rotor.ex
+++ lib/rotor.ex
@@ -1,5 +1,14 @@
 defmodule Rotor do
-  def translate_left(mapping, position, input), do: Enum.at(mapping, input) - ?A
+  def translate_left(mapping, position, input) do
+    size = Enum.count mapping
+    input
+    |> rotated_input(position, size)
+    |> translate_left(mapping)
+    |> rotated_output(position, size)
+  end
+  defp translate_left(input, mapping), do: Enum.at(mapping, input) - ?A
+  defp rotated_input(input, position, size), do: input + size - (position - ?A) |> rem(size)
+  defp rotated_output(output, position, size), do: output + (position - ?A) |> rem(size)
 
   def translate_right(mapping, position, input), do: Enum.find_index(mapping, &(&1 - ?A == input))
 
--- test/rotor_test.exs
+++ test/rotor_test.exs
@@ -2,23 +2,37 @@ defmodule RotorTest do
   use ExUnit.Case
   import Rotor
 
-  # Test rotor mapping (home position):
-  #   0 <- B <- A <- 0
-  #   1 <- D <- B <- 1
-  #   2 <- A <- C <- 2
-  #   3 <- C <- D <- 3
-
+  # Test rotor mapping (BDAC):
+  #   C <-> D
+  #   A <-> C
+  #   D <-> B
+  #   B <-> A
   setup do
     {:ok, [mapping: 'BDAC']}
   end
 
+  # Home position:
+  #   3 <- D <- 3
+  #   2 <- C <- 2
+  #   1 <- B <- 1
+  #   0 <- A <- 0
   test "translate_left maps positionally using the supplied letter mapping when in the home position", context do
-    result = translate_left context[:mapping], 'A', 1 # 1 -> B -> D -> 3
+    result = translate_left context[:mapping], ?A, 1 # 1 -> B -> D -> 3
+    assert result == 3
+  end
+
+  # Rotated to position C:
+  #   3 <- B <- 3
+  #   2 <- A <- 2
+  #   1 <- D <- 1
+  #   0 <- C <- 0
+  test "translate_left maps positionally using the supplied letter mapping when rotated", context do
+    result = translate_left context[:mapping], ?C, 2 # 2 -> A -> B -> 3
     assert result == 3
   end
 
   test "translate_right maps positionally backwards using the supplied letter mapping when in the home position", context do
-    result = translate_right context[:mapping], 'A', 2 # 2 -> A -> C -> 3
+    result = translate_right context[:mapping], ?A, 2 # 2 -> A -> C -> 3
     assert result == 3
   end
 
